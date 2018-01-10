06b81da38acdf63346c2ebb8591fda7b69e43512
Jens Axboe <axboe@kernel.dk>
2018-01-10 11:46:39 -0700

    block: rearrange a few request fields for better cache layout
    Move completion related items (like the call single data) near the
    end of the struct, instead of mixing them in with the initial
    queueing related fields.
    
    Move queuelist below the bio structures. Then we have all
    queueing related bits in the first cache line.
    
    This yields a 1.5-2% increase in IOPS for a null_blk test, both for
    sync and for high thread count access. Sync test goes form 975K to
    992K, 32-thread case from 20.8M to 21.2M IOPS.
    
    Reviewed-by: Bart Van Assche <bart.vanassche@wdc.com>
    Reviewed-by: Omar Sandoval <osandov@fb.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -151,13 +151,10 @@ typedef __u32 __bitwise req_flags_t;
  * especially blk_mq_rq_ctx_init() to take care of the added fields.
  */
 struct request {
-	struct list_head queuelist;
-	union {
-		struct __call_single_data csd;
-		u64 fifo_time;
-	};
+
 
 	struct request_queue *q;
+	struct list_head fg_list;
 	struct blk_mq_ctx *mq_ctx;
 
 	int cpu;
@@ -173,6 +170,8 @@ struct request {
 
 	struct bio *bio;
 	struct bio *biotail;
+
+	struct list_head queuelist;
 
 	/*
 	 * The hash is used inside the scheduler, and killed once the
@@ -221,11 +220,7 @@ struct request {
 	struct hd_struct *part;
 	unsigned long start_time;
 	struct blk_issue_stat issue_stat;
-#ifdef CONFIG_BLK_CGROUP
-	struct request_list *rl;		/* rl this rq is alloced from */
-	unsigned long long start_time_ns;
-	unsigned long long io_start_time_ns;    /* when passed to hardware */
-#endif
+
 	/* Number of scatter-gather DMA addr+len pairs after
 	 * physical address coalescing is performed.
 	 */
@@ -234,6 +229,7 @@ struct request {
 	unsigned short nr_integrity_segments;
 #endif
 
+	unsigned short write_hint;
 	unsigned short ioprio;
 
 	unsigned int timeout;
@@ -242,7 +238,7 @@ struct request {
 
 	unsigned int extra_len;	/* length of alignment and padding */
 
-	unsigned short write_hint;
+
 
 	/*
 	 * On blk-mq, the lower bits of ->gstate (generation number and
@@ -270,6 +266,11 @@ struct request {
 
 	struct list_head timeout_list;
 
+	union {
+		call_single_data_t csd;
+		u64 fifo_time;
+	};
+
 	/*
 	 * completion callback.
 	 */
@@ -281,6 +282,12 @@ struct request {
 
 	ktime_t			lat_hist_io_start;
 	int			lat_hist_enabled;
+
+#ifdef CONFIG_BLK_CGROUP
+	struct request_list *rl;		/* rl this rq is alloced from */
+	unsigned long long start_time_ns;
+	unsigned long long io_start_time_ns;    /* when passed to hardware */
+#endif
 };
 
 static inline bool blk_op_is_scsi(unsigned int op)
