ad509579ab25d903323ba8bb1130b3eb45af24b7
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-02-11 00:08:36 +0800

    Revert "usb: gadget: rndis: add spinlock for rndis response list"
    These are removed from https://github.com/LuanHalaiko/kernel_raphael_sm8150/commit/a9ce592f00e93 and
    https://github.com/RealJohnGalt/GaltsGulch-sm8150/blob/e715dc50803c09dc2a189005fbf31720332bb17a/drivers/usb/gadget/function/rndis.c
    
    This reverts commit 669c2b178956718407af5631ccbc61c24413f038.

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -947,7 +947,6 @@ struct rndis_params *rndis_register(void (*resp_avail)(void *v), void *v,
 	params->flow_ctrl_enable = flow_ctrl_enable;
 	params->v = v;
 	INIT_LIST_HEAD(&params->resp_queue);
-	spin_lock_init(&params->resp_lock);
 	pr_debug("%s: configNr = %d\n", __func__, i);
 
 	return params;
@@ -1128,7 +1127,6 @@ u8 *rndis_get_next_response(struct rndis_params *params, u32 *length)
 		}
 	}
 
-	spin_unlock(&params->resp_lock);
 	return NULL;
 }
 EXPORT_SYMBOL_GPL(rndis_get_next_response);
